9b3e2fbdc213d022bdc98495191333df81c100f6
jlewa <jlewandowska334@gmail.com>
2024-09-12 10:14:59 +0200

    Add a texture to the scene and an object

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,6 +2,10 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import * as dat from 'dat.gui';
 
+import norwat from '../img/norwat.jpg'; //importing an image
+import moon from '../img/moon.png'; //importing an image
+import castle from '../img/castle.jpg'; //importing an image
+
 const renderer = new THREE.WebGLRenderer();
 
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -28,7 +32,7 @@ const box = new THREE.Mesh( boxGeometry, boxMaterial );
 scene.add( box );
 box.position.set(0,10,0);
 
-const planeGeometry = new THREE.PlaneGeometry( 30, 30 );
+const planeGeometry = new THREE.PlaneGeometry( 50, 50 );
 const planeMaterial = new THREE.MeshStandardMaterial( { color: 0xffffff, side: THREE.DoubleSide } );
 const plane = new THREE.Mesh( planeGeometry, planeMaterial );
 scene.add( plane );
@@ -48,27 +52,67 @@ scene.add( sphere );
 sphere.position.set(-10,0,0);
 sphere.castShadow = true;
 
-const ambientLight = new THREE.AmbientLight(0x333333);
-//scene.add(ambientLight); adds ambient light to the scene (like a light bulb)
+const ambientLight = new THREE.AmbientLight(0x333333,100);
+scene.add(ambientLight); //adds ambient light to the scene (like a light bulb)
 
-const directionalLight = new THREE.DirectionalLight(0xffffff,10);  
-scene.add(directionalLight); //adds a directional light to the scene (like the sun)
-directionalLight.position.set(-30,50,0);
-directionalLight.castShadow = true;
-directionalLight.shadow.camera.bottom=-12;
+// const directionalLight = new THREE.DirectionalLight(0xffffff,10);  
+// scene.add(directionalLight); //adds a directional light to the scene (like the sun)
+// directionalLight.position.set(-30,50,0);
+// directionalLight.castShadow = true;
+// directionalLight.shadow.camera.bottom=-12;
 
-const dLightHelper = new THREE.DirectionalLightHelper(directionalLight,5);
-scene.add(dLightHelper); //in order to see the light
+// const dLightHelper = new THREE.DirectionalLightHelper(directionalLight,5);
+// scene.add(dLightHelper); //in order to see the light
 
-const dLightShadowHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
-scene.add(dLightShadowHelper);//in order to see the shadow
+// const dLightShadowHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
+// scene.add(dLightShadowHelper);//in order to see the shadow
+
+// const spotLight = new THREE.SpotLight(0xffffff);
+// scene.add(spotLight); //adds a spot light to the scene (like a flashlight)
+// spotLight.position.set(-100,100,0);
+// spotLight.castShadow = true;
+// spotLight.angle=0.2;
+
+// const sLightHelper = new THREE.SpotLightHelper(spotLight);
+// scene.add(sLightHelper); //in order to see the light
+
+scene.fog = new THREE.Fog(0x000000, 0, 200); // fog when the camera is far away
+// scene.fog - new THREE.FogExp2(0x000000, 0.2); // fog when the camera is far away (doeesnt work)
+
+// renderer.setClearColor(0x123545); //background color
+
+const textureLoader = new THREE.TextureLoader();
+scene.background = textureLoader.load(norwat); //background image
+
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+scene.background = cubeTextureLoader.load([ moon, moon, moon, moon, moon ,moon]); //background image
+
+const box2Geometry = new THREE.BoxGeometry(10,10,10);
+const box2Material = new THREE.MeshStandardMaterial( { 
+    //map: textureLoader.load(castle),
+} );
+const box2MultiMaterial = [
+    new THREE.MeshBasicMaterial({map: textureLoader.load(castle)}),
+    new THREE.MeshBasicMaterial({map: textureLoader.load(moon)}),
+    new THREE.MeshBasicMaterial({map: textureLoader.load(castle)}),
+    new THREE.MeshBasicMaterial({map: textureLoader.load(moon)}),
+    new THREE.MeshBasicMaterial({map: textureLoader.load(castle)}),
+    new THREE.MeshBasicMaterial({map: textureLoader.load(moon)}),
+]
+const box2 = new THREE.Mesh( box2Geometry, box2MultiMaterial );
+box2.position.set(10,10,10);
+box2.material.map = textureLoader.load(castle);
+scene.add( box2 );
 
 const gui = new dat.GUI();
 
 const options = {
-    sphereColor: 0xff0000,
-    wireframe: false,
-    speed: 0.01
+    sphereColor: 0xff0000, //sphere color
+    wireframe: false, // sphere wire
+    speed: 0.01, // speed of the sphere
+    angle: 47698 , // angle of the spot light 
+    penumbra: 32556, // penumbra of the spot light
+    intensity: 4435.9// intensity of the spot light
 }
 
 gui.addColor(options, 'sphereColor').onChange(function(e){
@@ -79,8 +123,10 @@ gui.add(options, 'wireframe').onChange(function(e){
     sphereMaterial.wireframe = e;
 });
 
-gui.add(options,'speed',0,0.1)
-
+gui.add(options,'speed',0,1);
+//gui.add(options, 'penumbra', 0, 100000)
+//gui.add(options,'intensity',0,100000); 
+//gui.add(options, 'angle', 0, 100000)
 
 let step = 0;
 
@@ -92,6 +138,11 @@ function animate()
 
     step+=options.speed;
     sphere.position.y=10*Math.abs(Math.sin(step));
+
+    //spotLight.angle=options.angle;
+    //spotLight.penumbra=options.penumbra;
+    //spotLight.intensity=options.intensity;
+    //sLightHelper.update();
 
     renderer.render( scene, camera );
 }
